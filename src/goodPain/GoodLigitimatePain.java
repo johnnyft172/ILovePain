8b814e7a7bb509fd0048ab0989bbd712ffbf23f8
jmintegr1 <jmintegr1@gmail.com>
2018-12-19 20:30:12 -0500

    I love good pain

---
+++ src/goodPain/GoodLigitimatePain.java
@@ -0,0 +1,11 @@
+package goodPain;
+
+public class GoodLigitimatePain {
+
+    public static void main(String[] agrs){
+        System.out.println("I love to get past the 40% pain threshold until I tap into my power, the 2nd wind and beyond!");
+    }
+}
+
+
+
